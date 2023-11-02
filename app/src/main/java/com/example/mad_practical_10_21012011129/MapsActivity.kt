24f3bd91b6e95b1fc6720596a42f58b026f2ac1e
Yuvraj <rathodyuvrajsinh2004@gmail.com>
2023-11-02 17:12:08 +0530

    Practical-10 cpmpleted

---
+++ app/src/main/java/com/example/mad_practical_10_21012011129/MapsActivity.kt
@@ -0,0 +1,47 @@
+package com.example.mad_practical_10_21012011129
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mad_practical_10_21012011129.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private var lat:Double = 0.0
+    private var log:Double = 0.0
+    private var title = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val obj = intent.getSerializableExtra("Object") as Person
+        lat = obj.latitude
+        log = obj.longitude
+        title = obj.name
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        val sydney = LatLng(lat,  log )
+        mMap.addMarker(MarkerOptions().position(sydney)
+            .title(title)
+            .snippet(title)
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.image))
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,16.0f))
+    }
+}
